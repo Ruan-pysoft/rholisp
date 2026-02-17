41b9172aec7a75da28706126aca686c63baba427
Ruan <ruan@pysoft.co.za>
2026-02-17 07:38:59 +0200

    Procedural dialect: for loop & bugfix
    1) Implemented for loop with break statement (in `procedural.lisp`)
    2) Fix return to wrap return value in a list, to guarantee it returns
       (previously `(return F)` wouldn't work)
    3) Write an iterative Fibonacci function in the test program to
       demonstrate working for loops

--- procedural.lisp
+++ procedural.lisp
@@ -3,17 +3,64 @@
   (def name value)
 )
 
+(defn .fun.for.resolve-expr (expr)
+  (if (and (= (type expr) ' list) expr (= (head expr) ' break))
+    (do
+      (assert (not (tail expr)))
+      T
+    )
+    (.fun.resolve-expr expr)
+  )
+)
+
+(defn .fun.for.transform (val)
+  (if (!= val T) val)
+)
+
 (defn .fun.resolve-expr (expr)
   ;(do (println "resolving expr:" expr)
   ;(println ".fun.resolve-expr:"
   (if (and (= (type expr) ' list) expr)
     (switch (head expr)
-      (case ' return (scd expr))
+      (case ' return (list ' list (scd expr)))
       (case ' if (do
         (assert (or (= (len expr) 3) (= (len expr) 4)))
         (if (= (len expr) 3)
           (list ' if (scd expr) (.fun.resolve-expr (nth expr 2)))
           (list ' if (scd expr) (.fun.resolve-expr (nth expr 2)) (.fun.resolve-expr (nth expr 3)))
+        )
+      ))
+      (case ' for (do
+        ; (for init cond next body...)
+        (if (< (len expr) 5) (do
+          (writle stderr "Expected at least four arguments supplied to for loop")
+          (exit 1)
+        ))
+        (if (= (len expr) 5)
+          (list ' env-new (list ' do
+            (list ' def ' .fun.for.rec (list ' \ () (
+              list ' if (nth expr 2)
+              (list ' or (.fun.for.resolve-expr (nth expr 4)) (list ' do (nth expr 3) ()) '(.fun.for.rec))
+            )))
+            (scd expr)
+            '(.fun.for.rec)
+          ))
+          (list ' .fun.for.transform
+            (list ' env-new (list ' do
+              (list ' def ' .fun.for.rec (list ' \ () (
+                list ' if (nth expr 2)
+                (append
+                  (append
+                    (cons ' or (map .fun.for.resolve-expr (tail (tail (tail (tail expr))))))
+                    (list ' do (nth expr 3) ())
+                  )
+                  '(.fun.for.rec)
+                )
+              )))
+              (scd expr)
+              '(.fun.for.rec)
+            ))
+          )
         )
       ))
       ; TODO: (do ...)
@@ -30,15 +77,22 @@
   ; )
 )
 
+(defn .fun.extract-val (retval)
+  "extract the function's return value from the list it's wrapped in if it isn't nil"
+  (if retval (head retval) retval)
+)
+
 (defm fun (name params () body)
   "define a function"
   (call defn (list
     ' name
     ' params
     "a procedural function"
-    (list ' env-new (cons ' or (cons () (map
-      .fun.resolve-expr
-      ' body
-    ))))
+    (list ' .fun.extract-val
+      (list ' env-new (cons ' or (cons () (map
+        .fun.resolve-expr
+        ' body
+      ))))
+    )
   ))
 )
